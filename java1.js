8f45f35869ef8a35229300a8b2b76b14deb3f4e5
fitri-pixel <heyirtif80@gmail.com>
2024-06-07 22:04:40 +0800

    Add files via upload

---
+++ java1.js
@@ -0,0 +1,24 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const img = document.querySelector("img");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+const secondQuestion document.querySelectorAll(".question")[1];
+
+yesBtn.addEventListener("click", () => { 
+  question.innerHTML = "I like you";
+  secondQuestion.innerHTML = "How about you?"; 
+  img.src =
+  
+  https://raw.githubusercontent.com/DzarelDeveloper/Img/main/AskCrush1.png"
+  
+  noBtn.addEventListener("mouseover", () => {
+    const noßtnRect = noßtn.getBoundingClientRect();
+    const maxx = window. InnerWidth - noßtnRect.width;
+    const maxy = window.innerHeight - noBtnRect.height;
+    
+    const randomX = Math.floor(Math.random() * maxx);
+    const randomy = Math.floor(Math.random() * maxy);
+    
+    noBtn.style.left = randomX + "px";
+    noBtn.style.top randomy + "px";
